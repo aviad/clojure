47dcdd513bb65f53077ed9a04aa154585d0a6496
Aviad Reich <avreich@cisco.com>
2015-12-24 18:15:35 +0200

    (CLJ-1871) Add the ability to rename columns in clojure.pprint/print-table

--- src/clj/clojure/pprint/print_table.clj
+++ src/clj/clojure/pprint/print_table.clj
@@ -11,25 +11,35 @@
 (defn print-table
   "Prints a collection of maps in a textual table. Prints table headings
    ks, and then a line of output for each row, corresponding to the keys
-   in ks. If ks are not specified, use the keys of the first item in rows."
+   in ks. If ks are not specified, use the keys of the first item in rows.
+   To rename headings ks, use a sequence of [key column-name] as ks."
   {:added "1.3"}
   ([ks rows]
-     (when (seq rows)
-       (let [widths (map
-                     (fn [k]
-                       (apply max (count (str k)) (map #(count (str (get % k))) rows)))
-                     ks)
-             spacers (map #(apply str (repeat % "-")) widths)
-             fmts (map #(str "%" % "s") widths)
-             fmt-row (fn [leader divider trailer row]
-                       (str leader
-                            (apply str (interpose divider
-                                                  (for [[col fmt] (map vector (map #(get row %) ks) fmts)]
-                                                    (format fmt (str col)))))
-                            trailer))]
-         (println)
-         (println (fmt-row "| " " | " " |" (zipmap ks ks)))
-         (println (fmt-row "|-" "-+-" "-|" (zipmap ks spacers)))
-         (doseq [row rows]
-           (println (fmt-row "| " " | " " |" row))))))
+   (when (seq rows)
+     (let [col-names-specified (sequential? (first ks))
+           col-names (if col-names-specified (map second ks) ks)
+           ks (if col-names-specified (map first ks) ks)
+           widths (map
+                   (fn [k]
+                     (apply max
+                            (count (str k))
+                            (map #(count (str (get % k))) rows)))
+                   ks)
+           spacers (map #(clojure.string/join (repeat % "-")) widths)
+           fmts (map #(str "%" % "s") widths)
+           fmt-row (fn [leader divider trailer row]
+                     (str leader
+                          (clojure.string/join
+                           (interpose divider
+                                      (for [[col fmt]
+                                            (map vector
+                                                 (map #(get row %) ks)
+                                                 fmts)]
+                                        (format fmt (str col)))))
+                          trailer))]
+       (println)
+       (println (fmt-row "| " " | " " |" (zipmap ks col-names)))
+       (println (fmt-row "|-" "-+-" "-|" (zipmap ks spacers)))
+       (doseq [row rows]
+         (println (fmt-row "| " " | " " |" row))))))
   ([rows] (print-table (keys (first rows)) rows)))
